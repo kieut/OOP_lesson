c880c7a35d84a619c6e04b68a4fe2653f721e306
Hackbright Student <info@hackbrightacademy.com>
2014-06-11 16:26:03 -0700

    add completed exercise for game

--- game.py
+++ game.py
@@ -6,9 +6,14 @@ import sys
 
 class Rock(GameElement):
     IMAGE = "Rock"
+    SOLID = True
  
 class Character(GameElement):
     IMAGE = "Princess"
+
+    def __init__(self):
+        GameElement.__init__(self)
+        self.inventory = []
 
     def next_pos(self, direction):
         if direction == "up":
@@ -20,6 +25,15 @@ class Character(GameElement):
         elif direction == "right":
             return (self.x+1, self.y)
         return None
+
+
+class Gem(GameElement):
+    IMAGE = "BlueGem"
+    SOLID = False
+
+    def interact(self, player):
+        player.inventory.append(self)
+        GAME_BOARD.draw_msg("You just acquired a gem! You have %d items!" %(len(player.inventory)))
 
 #### DO NOT TOUCH ####
 GAME_BOARD = None
@@ -72,17 +86,30 @@ def initialize():
         GAME_BOARD.set_el(pos[0], pos[1], rock)
         rocks.append(rock)
 
+    rocks[-1].SOLID = False
+
     for rock in rocks:
         print rock
 
+
     #intitalize global player
+
     global PLAYER
     PLAYER = Character()
     GAME_BOARD.register(PLAYER)
     GAME_BOARD.set_el(2, 2, PLAYER)
     print PLAYER
 
+
     GAME_BOARD.draw_msg("This game is wicked awesome.")
+
+    gem = Gem()
+    GAME_BOARD.register(gem)
+    GAME_BOARD.set_el(3,1,gem)
+
+    gem2 = Gem()
+    GAME_BOARD.register(gem2)
+    GAME_BOARD.set_el(0,3,gem2)
 
 def keyboard_handler():
     # if KEYBOARD[key.UP]:
@@ -124,5 +151,15 @@ def keyboard_handler():
         next_x = next_location[0]
         next_y = next_location[1]
 
-        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-        GAME_BOARD.set_el(next_x, next_y, PLAYER)
+        existing_el = GAME_BOARD.get_el(next_x, next_y)
+
+        if existing_el:
+            existing_el.interact(PLAYER)
+
+        if existing_el is None or not existing_el.SOLID:
+            GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+            GAME_BOARD.set_el(next_x, next_y, PLAYER)
+
+        
+        
+
